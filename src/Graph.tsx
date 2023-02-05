657d39c14c1c2bb23e2ff345f918b92a52905348
Pranav V <100434347+jainvpranav@users.noreply.github.com>
2023-02-05 21:14:22 +0530

    Updated Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -27,6 +27,9 @@ class Graph extends Component<IProps, {}> {
       top_ask_price: 'float',
       top_bid_price: 'float',
       timestamp: 'date',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -38,7 +41,7 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
         stock: 'distinctcount',
         top_ask_price: 'avg',
@@ -50,8 +53,9 @@ class Graph extends Component<IProps, {}> {
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
+        ] as unknown as TableData
       );
     }
   }
